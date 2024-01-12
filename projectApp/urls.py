9d5571349ff723e530bab33dd388ac70f60f3a8e
Tiffany <Tiffany2016d@gmail.com>
2024-01-12 16:03:32 +0000

    inline styling with post functions

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path("home/", views.home, name="home"),
-    path("inputMolecules/", views.inputMolecules, name="inputMolecules"),
-    path("inputOneMolecule/", views.inputOneMolecule, name="inputOneMolecule"),
-    path("inputTwoMolecules/", views.inputTwoMolecules, name="inputTwoMolecules")
-]
+    path("oneMolecule/", views.oneMolecule, name="oneMolecule"),
+    path("views.twoMolecules/", views.twoMolecules, name="twoMolecules"),
+    path("collectDataOne/", views.collectInputOne, name="collectInputOne"),
+    path("collectDataTwo/", views.collectInputTwo, name="collectInputTwo"),
+    path("processData/", views.processData, name="processData")
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- projectApp/views.py
+++ projectApp/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .forms import oneMolecule
+from subunitIdent import subunitIdent
 
 def home(request):
     return render(request, "projectApp/home.html")
 
-def inputMolecules(request):
-    molecule = request.POST.get("molecule")
-    print(molecule)
-    return HttpResponse("Recieved molecule!")
-
-def inputOneMolecule(request):
+def oneMolecule(request):
     return render(request, "projectApp/oneMolecule.html")
 
-def inputTwoMolecules(request):
-    return render(request, "projectApp/twoMolecules.html")
+def twoMolecules(request):
+    return render(request, "projectApp/TwoMolecules.html")
+
+def collectInputOne(request):
+    return render(request, 'projectApp/oneMolecule.html')
+
+def collectInputTwo(request):
+    return render(request, 'projectApp/twoMolecules.html')
+
+def processData(request):
+    input = request.POST.get("user-input")
+    output = subunitIdent(input)
+    return render(request, "projectApp/oneMolecules.html", {"molecular_data": output.molecular_data})
